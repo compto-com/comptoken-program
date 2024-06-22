bd9d6a70340cca6cd9660182fd6ecf8b4dbdad8c
connor-compto <connor@compto.com>
2024-06-22 12:59:57 -0500

    seperate autogenerated values (#4)
    * seperate autogenerated values
    
    * switch to fully autogenerated, ignored file
    
    * create .cache if it doesn't exist
    
    * fix solana-test-validator surviving as a zombie process
    
    ---------
    
    Co-authored-by: David Trost <david@compto.com>

--- src/comptoken.rs
+++ src/comptoken.rs
@@ -7,7 +7,6 @@ use solana_program::{
     entrypoint::ProgramResult,
     pubkey::Pubkey,
     msg,
-    pubkey,
     program::invoke_signed,
     sysvar,
     hash::Hash,
@@ -15,25 +14,11 @@ use solana_program::{
     // sysvar::{slot_hashes::SlotHashes, Sysvar},
 };
 use spl_token::instruction::mint_to;
-use hex::encode;
 // declare and export the program's entrypoint
 entrypoint!(process_instruction);
 
-// vvv This line is automatically updated by full_deploy_test.py.
-static COMPTOKEN_ADDRESS: Pubkey = pubkey!("9cc6aPPuknsG83y8KUM2uVwZ7XHoagYwxNsNEzxomypi");
-// ^^^ DO NOT TOUCH. ^^^
-
-// A given seed and program id have a 50% chance of creating a valid PDA.
-// Before building/deploying, we find the canonical seed by running 
-//      `solana find-program-derived-address <program_id>`
-// This is an efficiency optimization. We are using a static seed to create the PDA with no bump.
-// We ensure when deploying that the program id is one that only needs the seed above and no bump.
-// This is because 
-//      (1) create_program_address is not safe if using a user provided bump.
-//      (2) find_program_address is expensive and we want to avoid iterations.
-// vvv This line is automatically updated by full_deploy_test.py.
-static COMPTO_STATIC_ADDRESS_SEED: u8 = 255;
-// ^^^ DO NOT TOUCH. ^^^
+mod comptoken_generated;
+use comptoken_generated::{COMPTOKEN_ADDRESS, COMPTO_STATIC_ADDRESS_SEED};
 
 
 // #[derive(Debug, Default, BorshDeserialize, BorshSerialize)]

36d7e0b1217dbfc340a429ad8787761e46e08221
connor-compto <connor@compto.com>
2024-07-26 21:35:29 -0500

    Test get valid blockhashes (#32)
    * add basic size check in GlobalData from
    
    * move BackgroundProcess to top of full_deploy_test.py
    
    * create SPL_TOKEN_CMD const
    
    * minor edit to common.js
    
    * add solana-bankrun node dependency
    
    * rename testuser_keypair to testUser_keypair
    
    * add assert.js
    
    * add test_mint.js
    
    * add accounts.js
    
    * fix bugs
    
    * comments
    
    * add initializeComptokenProgram
    
    * comments
    
    * add initializeComptokenProgram
    
    * add test_mint.js
    
    * fix bugs
    
    * comments
    
    * add initializeComptokenProgram
    
    * add deserialization functions
    
    * move DailyDistributionValues to daily_distribution_data.rs
    
    * add custom message to asserts
    
    * export everything
    
    * add asserts to initialize_comptoken_program
    
    * add UserDataAccount class in accounts.js
    
    * update comments
    
    * add default times
    
    * add test_getValidBlockhashes
    
    * minor formatting edit
    
    * give test_getValidBlockhashes a .js extension and remove unused imports
    
    * minor edits to comptoken_proof.js and accounts.js
    
    * fixes to accounts.js + isArrayEqual()
    
    * give default globalData non-zero valid/announced blockhashes
    
    * minor formatting edit
    
    * give test_getValidBlockhashes a .js extension and remove unused imports
    
    * give default globalData non-zero blockhashes
    
    * add test_getValidBlockhashes
    
    * minor formatting edit
    
    * give test_getValidBlockhashes a .js extension and remove unused imports
    
    * add test_getValidBlockhashes
    
    * give test_getValidBlockhashes a .js extension and remove unused imports
    
    * give default globalData non-zero blockhashes
    
    * fix some bugs with framework
    
    * fix a bug in daily distribution

---
+++ test/compto-test-client/test_getValidBlockhashes.js
@@ -0,0 +1,42 @@
+import { SYSVAR_SLOT_HASHES_PUBKEY, Transaction, TransactionInstruction } from "@solana/web3.js";
+import { Clock, start } from "solana-bankrun";
+
+import { get_default_comptoken_mint, get_default_global_data, programId } from "./accounts.js";
+import { Assert } from "./assert.js";
+import { DEFAULT_START_TIME, global_data_account_pubkey, Instruction } from "./common.js";
+
+async function test_getValidBlockhashes() {
+    let globalData = get_default_global_data();
+
+    const context = await start(
+        [{ name: "comptoken", programId }],
+        [
+            get_default_comptoken_mint().toAccount(),
+            globalData.toAccount(),
+        ]
+    );
+
+    const client = context.banksClient;
+    const payer = context.payer;
+    const blockhash = context.lastBlockhash;
+    const rent = await client.getRent();
+    const keys = [
+        // stores valid blockhashes, but may be out of date
+        { pubkey: global_data_account_pubkey, isSigner: false, isWritable: true },
+        // program will pull a recent hash from slothashes sysvar if a new valid blockhash is needed.  
+        { pubkey: SYSVAR_SLOT_HASHES_PUBKEY, isSigner: false, isWritable: false },
+    ];
+
+    const ixs = [new TransactionInstruction({ programId, keys, data: Buffer.from([Instruction.GET_VALID_BLOCKHASHES]) })];
+    const tx = new Transaction();
+    tx.recentBlockhash = blockhash;
+    tx.add(...ixs);
+    tx.sign(payer);
+    context.setClock(new Clock(0n, 0n, 0n, 0n, DEFAULT_START_TIME));
+    const meta = await client.processTransaction(tx);
+    const validBlockHashes = { current_block: meta.returnData.data.slice(0, 32), announced_block: meta.returnData.data.slice(32, 64), };
+    Assert.assert(validBlockHashes.announced_block.every((v, i) => v === globalData.validBlockhashes.announcedBlockhash[i]), "announced blockhash is globalData default");
+    Assert.assert(validBlockHashes.current_block.every((v, i) => v === globalData.validBlockhashes.validBlockhash[i]), "valid blockhash is globalData default");
+}
+
+(async () => { await test_getValidBlockhashes(); })();

e1ae5839b6d9e205cb3dc753a30f66a8e945a1af
connor-compto <connor@compto.com>
2024-08-12 16:58:38 -0500

    switch execute to new system

--- test/compto-test-client/transfer-hook-tests/execute.js
+++ test/compto-test-client/transfer-hook-tests/execute.js
@@ -1,6 +1,5 @@
 import { TOKEN_2022_PROGRAM_ID, TokenInstruction, transferCheckedInstructionData } from "@solana/spl-token";
-import { Keypair, PublicKey, Transaction, TransactionInstruction } from "@solana/web3.js";
-import { Clock, start } from "solana-bankrun";
+import { Keypair, PublicKey, TransactionInstruction } from "@solana/web3.js";
 
 import {
     get_default_comptoken_mint,
@@ -10,39 +9,33 @@ import {
     TokenAccount
 } from "../accounts.js";
 import { Assert } from "../assert.js";
-import { compto_program_id_pubkey, compto_transfer_hook_id_pubkey, COMPTOKEN_DECIMALS, DEFAULT_START_TIME } from "../common.js";
+import { compto_program_id_pubkey, compto_transfer_hook_id_pubkey, COMPTOKEN_DECIMALS } from "../common.js";
+import { get_account, run_test, setup_test } from "../generic_test.js";
 
 async function test_execute() {
-    console.log("test execute")
-    let comptoken_mint = get_default_comptoken_mint();
-    const user1_owner = Keypair.generate()
+    const user1_owner = Keypair.generate();
+
+    const comptoken_mint = get_default_comptoken_mint();
     let user1 = get_default_comptoken_wallet(PublicKey.unique(), user1_owner.publicKey);
     user1.data.amount = 1n;
-    let user2 = get_default_comptoken_wallet(PublicKey.unique(), PublicKey.unique());
-    let user1_data = get_default_user_data_account(PublicKey.findProgramAddressSync([user1.address.toBytes()], compto_program_id_pubkey)[0]);
-    let user2_data = get_default_user_data_account(PublicKey.findProgramAddressSync([user2.address.toBytes()], compto_program_id_pubkey)[0]);
-    let extraAccountMetaAccount = get_default_extra_account_metas_account();
+    const user1_data = get_default_user_data_account(PublicKey.findProgramAddressSync([user1.address.toBytes()], compto_program_id_pubkey)[0]);
 
-    const context = await start(
-        [
-            { name: "comptoken", programId: compto_program_id_pubkey },
-            { name: "comptoken_transfer_hook", programId: compto_transfer_hook_id_pubkey },
-        ],
-        [
-            user1.toAddedAccount(),
-            comptoken_mint.toAddedAccount(),
-            user2.toAddedAccount(),
-            extraAccountMetaAccount.toAddedAccount(),
-            user1_data.toAddedAccount(),
-            user2_data.toAddedAccount(),
-        ]
-    );
+    const user2 = get_default_comptoken_wallet(PublicKey.unique(), PublicKey.unique());
+    const user2_data = get_default_user_data_account(PublicKey.findProgramAddressSync([user2.address.toBytes()], compto_program_id_pubkey)[0]);
 
-    const client = context.banksClient;
-    const payer = context.payer;
-    const blockhash = context.lastBlockhash;
+    const accounts = [
+        comptoken_mint,
+        user1,
+        user1_data,
+        user2,
+        user2_data,
+        get_default_extra_account_metas_account(),
+    ];
 
-    client.getAccountInfo = client.getAccount;
+    let context = await setup_test(accounts);
+
+    // solana/web3.js createTransferCheckedInstructionWithTransferHook requires a connection, which bankrun replaces with a mock
+    // with a similar interface. We can't use it here, so we'll create the instruction manually.
     const keys = [
         // transfer keys
         { pubkey: user1.address, isSigner: false, isWritable: true },
@@ -50,7 +43,7 @@ async function test_execute() {
         { pubkey: user2.address, isSigner: false, isWritable: true },
         { pubkey: user1_owner.publicKey, isSigner: true, isWritable: false },
         // transfer hook api keys
-        { pubkey: extraAccountMetaAccount.address, isSigner: false, isWritable: false },
+        { pubkey: get_default_extra_account_metas_account().address, isSigner: false, isWritable: false },
         // our transfer hook keys
         { pubkey: compto_program_id_pubkey, isSigner: false, isWritable: false },
         { pubkey: user1_data.address, isSigner: false, isWritable: false },
@@ -68,26 +61,17 @@ async function test_execute() {
         },
         data
     );
-    const ixs = [
-        new TransactionInstruction({ programId: TOKEN_2022_PROGRAM_ID, keys, data })
-    ];
-    const tx = new Transaction();
-    tx.recentBlockhash = blockhash;
-    tx.add(...ixs);
-    tx.feePayer = payer.publicKey;
-    tx.sign(payer, user1_owner);
-    context.setClock(new Clock(0n, 0n, 0n, 0n, DEFAULT_START_TIME));
-    const meta = await client.processTransaction(tx);
 
-    console.log("logMessages: %s", meta.logMessages);
-    console.log("computeUnitsConsumed: %d", meta.computeUnitsConsumed);
-    console.log("returnData: %s", meta.returnData);
+    let instructions = [new TransactionInstruction({ programId: TOKEN_2022_PROGRAM_ID, keys, data })];
+    let result;
 
-    let account = await client.getAccount(user2.address);
-    Assert.assertNotNull(account);
-    let finalUser2 = TokenAccount.fromAccountInfoBytes(user2.address, account);
-    Assert.assertEqual(finalUser2.data.amount, 1n);
+    [context, result] = await run_test("execute", context, instructions, [context.payer, user1_owner], async (context, result) => {
+        const finalUser1 = await get_account(context, user1.address, TokenAccount);
+        Assert.assertEqual(finalUser1.data.amount, 0n);
 
+        const finalUser2 = await get_account(context, user2.address, TokenAccount);
+        Assert.assertEqual(finalUser2.data.amount, 1n);
+    });
 }
 
 (async () => { await test_execute(); })();

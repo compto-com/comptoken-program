1ff4adaed880f2dca5434457aa8cbe2b14240498
connor-compto <connor@compto.com>
2024-08-14 21:09:47 -0500

    finish renaming

--- test/compto-test-client/comptoken-tests/earlyDailyDistributionEvent.js
+++ test/compto-test-client/comptoken-tests/earlyDailyDistributionEvent.js
@@ -3,7 +3,7 @@ import {
     get_default_global_data,
     get_default_unpaid_future_ubi_bank,
     get_default_unpaid_interest_bank,
-    get_default_unpaid_ubi_bank,
+    get_default_unpaid_verified_human_ubi_bank,
     MintAccount,
 } from "../accounts.js";
 import { Assert } from "../assert.js";
@@ -16,7 +16,7 @@ async function test_earlyDailyDistributionEvent() {
     original_comptoken_mint.data.supply += 1n;
 
     const existing_accounts = [
-        original_comptoken_mint, get_default_global_data(), get_default_unpaid_interest_bank(), get_default_unpaid_ubi_bank(), get_default_unpaid_future_ubi_bank(),
+        original_comptoken_mint, get_default_global_data(), get_default_unpaid_interest_bank(), get_default_unpaid_verified_human_ubi_bank(), get_default_unpaid_future_ubi_bank(),
     ];
 
     let context = await setup_test(existing_accounts);

2d3c9f6e07305ead2eb2ea0ebdc7a83523c8c0d0
connor-compto <connor@compto.com>
2025-01-22 14:48:58 -0600

    target master branch of comptoken.js

--- test/compto-test-client/test_client.js
+++ test/compto-test-client/test_client.js
@@ -42,7 +42,7 @@ console.log("comptoken mint: " + compto_public_keys.comptoken_mint_pubkey);
 console.log("compto program id: " + compto_public_keys.compto_program_id_pubkey);
 console.log("global data account: " + compto_public_keys.global_data_account_pubkey);
 
-let connection = new Connection('http://localhost:8899', 'recent');
+let connection = new Connection('http://localhost:8899', 'confirmed');
 
 (async () => {
     await airdrop(compto_public_keys.test_account.publicKey);

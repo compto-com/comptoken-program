3e8b3e545f48477863d2405b2430a9066feb959a
connor-compto <connor@compto.com>
2026-07-31 13:48:45 -0500

    feat: continue adding v4

--- programs/comptoken/src/state/nullifier.rs
+++ programs/comptoken/src/state/nullifier.rs
@@ -5,5 +5,11 @@ pub struct Nullifier {
     pub user_wallet: Pubkey,
 }
 
+// TODO: can nullifiers be removed in v4? they are only useful to prevent an attacker from reusing a proof to unverify a user,
+// (or verify them again after they unverify). trying to use the same proof to verify a second account will fail b/c the
+// session_id will be in use and proofs are only valid for a short period of time so reverification is not a concern
+// I believe this attack can be prevented by using a different signal for the different functions and therefore making each
+// proof unique to the function.
+
 #[account()]
 pub struct NullifierV4 {}
